02c188d959b4064611bd0589c11eaf973053cc61
devkk363 <49581434+devkk363@users.noreply.github.com>
2019-04-17 11:37:31 +0530

    Update pos_neg.cpp

--- pos_neg.cpp
+++ pos_neg.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int main() {
-int n;
-
-cin>>n;
-
-if(n== 0)
-  cout<<"Zero";
-else if(n>0)
-cout<<"Positive";
-else
-  cout<<"Negative";
-
-	// your code goes here
-	return 0;
+int main(){
+    
+    int num;
+    
+    cin>> num;
+    
+    if(num>0)
+        cout<<"Positive"<<endl;
+    else if(num<0)
+        cout<<"Negative"<<endl;
+    else
+        cout<<"Zero"<<endl;
 }
