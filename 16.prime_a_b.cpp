68544f5886a557cf2cded9e5d41691e6253b9ca4
devkk363 <49581434+devkk363@users.noreply.github.com>
2019-04-18 14:45:42 +0530

    Update 16.prime_a_b.cpp

--- 16.prime_a_b.cpp
+++ 16.prime_a_b.cpp
@@ -7,18 +7,19 @@ int main(){
     int num1,num2, flag;
     
     cin>>num1>>num2;
-    
+    int n = num1;
     while(num1<num2){
         flag = 0;
         
         for(int i=2; i<= num1/2;i++){
-            if( num1%i == 0){
+            if(num1%i == 0){
                 flag = 1;
                 break;
             }
             
         }
-        if(num1>1 && flag == 0)
+        
+        if(num1>n && flag == 0)
             cout << num1 << " ";
         
         num1++;
