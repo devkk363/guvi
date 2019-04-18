2d0b181677e721d16057f8cb480129c5864f66a3
devkk363 <49581434+devkk363@users.noreply.github.com>
2019-04-18 11:32:42 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, orgInt, revInt = 0 ,rem;
+    
+    cin >> n;
+    orgInt = n;
+    
+    while(n!=0){
+        rem = n%10; 
+        revInt = revInt*10 + rem; 
+        n/=10; 
+    }
+    
+    if(orgInt == revInt)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+    
+}
+
