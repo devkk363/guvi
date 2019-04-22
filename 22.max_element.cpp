930b709f0a7f2899ec0dd62861d1f8e833d444db
devkk363 <49581434+devkk363@users.noreply.github.com>
2019-04-22 16:45:17 +0530

    Create 22.max_element.cpp

---
+++ 22.max_element.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n,a=0,b;
+    
+    cin >> n;
+    int i =1;
+    while(i <= n){
+        
+        cin >> b;
+        
+        if(b>a)
+            a = b;
+        i++;   
+    }
+    
+    cout<<a;
+}
