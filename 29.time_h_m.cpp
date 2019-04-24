a3b1bb7e16fe279994b7605f7baea91172357caf
devkk363 <49581434+devkk363@users.noreply.github.com>
2019-04-24 17:12:23 +0530

    Update 29.time_h_m.cpp

--- 29.time_h_m.cpp
+++ 29.time_h_m.cpp
@@ -7,11 +7,11 @@ int main(){
     int num,h,m;
     
     cin>>num ; //52;
-    
+    if(num<0) cout<<"Not a vaild number "; return -1;
     if(num!=0){
             h = num/60;
             m = num%60;    
     }
     
-    cout<<h<<" "<<m;
+    cout<<h<<":"<<m;
 }
