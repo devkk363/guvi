5121b57b4b216bba8499a991c6b01b4cecada15f
devkk363 <49581434+devkk363@users.noreply.github.com>
2019-04-24 18:29:54 +0530

    Create 27.numeric_not.cpp

---
+++ 27.numeric_not.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char ch;
+    
+    cin >>ch;
+    
+    if(ch >= 48 && ch<=57) 
+        cout<<"Yes";
+    else
+        cout<<"No";
+    
+}
